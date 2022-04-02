805785e6992e39438b7333c9d9f0cca12f52e159
basofiibnu <>
2022-04-02 12:24:49 +0700

    added new download and features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { Download, Features, SectionWrapper } from './components';
 import assets from './assets';
+import styles from './styles/global';
 
 const App = () => {
   return (
@@ -18,6 +19,26 @@ const App = () => {
         mockupImage={assets.homeCards}
         reverse
       />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNeft is built using Expo which runs natively on all user's devices. You can easily get your app into people's hands"
+        mockupImage={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImage={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by{' '}
+          <span className="font-bold">basofiibnu</span>
+        </p>
+      </div>
     </Fragment>
   );
 };
