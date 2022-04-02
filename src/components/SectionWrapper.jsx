2ed6712b2b9636a431619d346977a55ced7d0ff5
basofiibnu <>
2022-04-02 12:26:53 +0700

    added download link

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -41,7 +41,12 @@ const SectionWrapper = ({
           >
             {description}
           </p>
-          {showBtn && <Button assetUrl={assets.expo} link="" />}
+          {showBtn && (
+            <Button
+              assetUrl={assets.expo}
+              link="https://expo.dev/@basofiibnu/react_native_nft?serviceType=classic&distribution=expo-go"
+            />
+          )}
         </div>
 
         <div
