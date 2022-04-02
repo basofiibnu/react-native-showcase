d159b8e78dfcca78b0d08bdfc9070046b9c030fc
basofiibnu <>
2022-04-02 12:01:29 +0700

    added new banner and promo section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace"
+        showBtn
+        mockupImage={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant of a fluent UI design"
+        mockupImage={assets.homeCards}
+        reverse
+      />
+    </Fragment>
   );
-}
+};
 
 export default App;
